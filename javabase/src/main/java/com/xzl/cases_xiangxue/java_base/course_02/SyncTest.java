b2b1eb655bd9bb86a90b24719589fd3bc27ae1aa
徐路明 <xuluming@zailingtech.com>
2020-02-05 22:43:50 +0800

    [change] ...

---
+++ javabase/src/main/java/com/xzl/cases_xiangxue/java_base/course_02/SyncTest.java
@@ -0,0 +1,57 @@
+package com.xzl.cases_xiangxue.java_base.course_02;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SyncTest {
+
+    private static AtomicInteger mInt = new AtomicInteger(0);
+    private static Object lockObj = new Object();
+    public static void main(String[] args) throws InterruptedException {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("a000000000000000");
+                synchronized (lockObj){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("a11111111111111");
+                }
+
+                System.out.println("a22222222222222222");
+
+                System.out.println("a33333333333333333");
+                synchronized (lockObj){
+                    System.out.println("a44444444444444");
+                }
+                System.out.println("a55555555555555555");
+            }
+        }).start();
+
+
+        Thread.sleep(1);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("b000000000000000");
+                synchronized (lockObj){
+                    System.out.println("b11111111111111");
+                }
+
+                System.out.println("b22222222222222222");
+
+                System.out.println("b33333333333333333");
+                synchronized (lockObj){
+                    System.out.println("b44444444444444");
+                }
+                System.out.println("b55555555555555555");
+            }
+        }).start();
+
+    }
+}
